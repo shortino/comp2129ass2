b0bac054b928558a0be6da7f612e468289621576
James Alexander <shortino29@gmail.com>
2011-04-07 11:57:43 +1000

    input 2

--- dw.c
+++ dw.c
@@ -6,8 +6,19 @@
 #include "dw.h"
 
 
+
+
 int
 main(void) {
+
+	int numberOfRooms, scan;
+
+	/*Read in rooms and no of doughnuts and milkshakes*/
+	scan = scanf("%d", &numberOfRooms);	
+	
+	printf("this is %d\n", scan);
+
+
 	
 	return 0;
 }
