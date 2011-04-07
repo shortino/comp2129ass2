d7ff990e26b1f2f87b174a7a160d4959b2fa412e
James Alexander <shortino29@gmail.com>
2011-04-07 21:57:47 +1000

    added first ver of input

--- dw.c
+++ dw.c
@@ -11,16 +11,18 @@ void
 createRooms(int rooms){
 	struct list *curr;
 	struct room *new_room;
-	char name[21];
+	char names[21];
 	int scan, i, num_doughnuts, num_milkshakes, length;
 
 	for (i = 0; i != rooms; ++i){
-		scan = scanf("%c %d %d", name, &num_doughnuts, &num_milkshakes);
-		length = strlen(name);
+	
+		new_room = malloc(sizeof(struct room));
+		scan = scanf("%20s %d %d", names, &num_doughnuts, &num_milkshakes);
+		length = strlen(names);
 		if (scan && num_doughnuts > 0 && num_milkshakes > 0 && length < 21) {
 			curr = malloc(sizeof(struct list));
-			new_room = malloc(sizeof(struct room));
-			new_room->name = name;
+			strcpy(new_room->name, names);
+			//new_room->name = names;
 			new_room->num_doughnuts = num_doughnuts;
 			new_room->num_milkshakes = num_milkshakes;
 			curr->room = new_room;
@@ -29,9 +31,12 @@ createRooms(int rooms){
 		}
 		else { printf("error\n"); exit(1); } 
 	}
+	curr = head;
+	while(curr){
+		printf("%s %i %i\n", curr->room->name, curr->room->num_doughnuts, curr->room->num_milkshakes);
+		curr = curr->next;
+	}
 }
-
-
 
 
 int
@@ -41,7 +46,7 @@ main(void) {
 	int i;
 
 	/*Read in rooms and no of doughnuts and milkshakes*/
-	scan = scanf("%d", &number_ooms);	
+	scan = scanf("%d", &number_rooms);	
 	if (scan){	
 		createRooms(number_rooms);
 		printf("this is %d\n", scan);
