8a89223f9618f180a28a19b3dac3b2110e41b135
James Alexander <shortino29@gmail.com>
2011-04-07 21:12:31 +1000

    changes mand

--- dw.c
+++ dw.c
@@ -5,18 +5,49 @@
 
 #include "dw.h"
 
+struct list *head;
+
+void 
+createRooms(int rooms){
+	struct list *curr;
+	struct room *new_room;
+	char name[21];
+	int scan, i, num_doughnuts, num_milkshakes, length;
+
+	for (i = 0; i != rooms; ++i){
+		scan = scanf("%c %d %d", name, &num_doughnuts, &num_milkshakes);
+		length = strlen(name);
+		if (scan && num_doughnuts > 0 && num_milkshakes > 0 && length < 21) {
+			curr = malloc(sizeof(struct list));
+			new_room = malloc(sizeof(struct room));
+			new_room->name = name;
+			new_room->num_doughnuts = num_doughnuts;
+			new_room->num_milkshakes = num_milkshakes;
+			curr->room = new_room;
+			curr->next = head;
+			head = curr;
+		}
+		else { printf("error\n"); exit(1); } 
+	}
+}
+
 
 
 
 int
 main(void) {
 
-	int numberOfRooms, scan;
+	int number_rooms, scan;
+	int i;
 
 	/*Read in rooms and no of doughnuts and milkshakes*/
-	scan = scanf("%d", &numberOfRooms);	
-	
-	printf("this is %d\n", scan);
+	scan = scanf("%d", &number_ooms);	
+	if (scan){	
+		createRooms(number_rooms);
+		printf("this is %d\n", scan);
+	}
+	else { printf("error\n"); exit(1); } 
+
 
 
 	
