6159c177a84e7baee215c6dd1cfe94124b8256c6
James Alexander <shortino29@gmail.com>
2011-04-08 23:08:36 +1000

    final version before submission test case 6 still not working

--- dw-backup.c
+++ dw-backup.c
@@ -1,10 +1,3 @@
-/*
- * Comp2129 Assignment 2
- * James Alexander
- * 307192962
- *
- */
-
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,7 +5,6 @@
 
 #include "dw.h"
 
-/*Global Vars including the head and curr pointers for linked list */
 struct list *head;
 struct list *curr;
 char start_room[20];
@@ -20,34 +12,34 @@ char exit_room[20];
 int player_doughnuts;
 int player_milkshakes;
 
-/*Creates the rooms via a linear linked list*/
 int
 createRooms(int rooms){
-	struct room *new_room; //Tempoary room pointer
+	struct room *new_room;
 	char names[21];
 	int scan, i, num_doughnuts, num_milkshakes, length;
 	for (i = 0; i != rooms; ++i){
 		new_room = malloc(sizeof(struct room));
 		scan = scanf("%20s %d %d", names, &num_doughnuts, &num_milkshakes);//todo still need length?
 		length = strlen(names);
-		if (scan && num_doughnuts >= 0 && num_milkshakes >= 0 && length < 21) {//Input and boundary checking
+		if (scan && num_doughnuts >= 0 && num_milkshakes >= 0 && length < 21) {
 			curr = malloc(sizeof(struct list));
 			strcpy(new_room->name, names);
-			new_room->num_doughnuts = num_doughnuts; //Creates tempoary room
+			new_room->num_doughnuts = num_doughnuts;
 			new_room->num_milkshakes = num_milkshakes;
-			curr->room = new_room; //Sets curr's room to equal
-			curr->next = head; //Sets next to head and then head to curr
+			curr->room = new_room;
+			curr->next = head;
 			head = curr;
 		}
 		else { printf("error\n"); return 0; } 
 	}
+
 	return 1;
+
 }
 
-/*Returns enum type for directions, opp. refers to 2nd way direction*/
 enum direction getPath(char path, int opposite){
 	if(opposite){
-if (path == 'L') return RIGHT;
+		if (path == 'L') return RIGHT;
 		if (path == 'R') return LEFT;
 		if (path == 'U') return DOWN;
 		if (path == 'D') return UP;
@@ -61,7 +53,6 @@ if (path == 'L') return RIGHT;
 	return 0;
 }
 
-/*Creates and assigns doors to each room in the linked list*/
 int
 createDoors(int doors){
 	int i, scan, num, counter;
@@ -70,9 +61,9 @@ createDoors(int doors){
 	char path;
 	int path_one;
 	int path_two;
-	struct room *linked_room_one;	//Refernces to linked rooms which are then set outside the while loop
+	struct room *linked_room_one;
 	struct room *linked_room_two;
-	counter = 0;					//ensures correct number of assignments 
+	counter = 0;
 	for (i = 0; i != doors; ++i){
 		scan = scanf("%s %c %s", room_one, &path, room_two);
 		if (scan){
@@ -87,26 +78,33 @@ createDoors(int doors){
 					linked_room_one = curr->room;
 					path_one = getPath(path, 0);
 					counter++;
+					//printf("room oned %s\n", linked_room_one->name);
 				}
 				if (!strcmp(curr->room->name, room_two)){
 					linked_room_two = curr->room;
 					path_two = getPath(path, 1);
 					counter++;
+					//printf("room two is here after set %s\n", curr->room->name);
 				}
 
 				curr = curr->next;
 			}
 			/*Set the doors*/
 			linked_room_one->doors[path_one] = linked_room_two;
+			//printf("room one %s\n", linked_room_one->doors[path_one]->name);
 			linked_room_two->doors[path_two] = linked_room_one;		
+			//printf("room two %s\n", linked_room_two->doors[path_two]->name);
 		}
 		else { printf("error\n"); return 0; } 
 	}
-	if (counter < 2*doors) { printf("error\n"); return 0; } //Door not a previously defined room
+	if (counter < 2*doors) { printf("error\n"); return 0; } //Door not previously defined room
+
 	return 1;
 }
 
-/*Simple method to print required output inbetween commands*/
+
+
+
 void
 printInfo(void){
 	printf("%s %d %d %d %d\n", 
@@ -117,7 +115,6 @@ printInfo(void){
 			player_milkshakes);
 }
 
-/*Checks if current room is the exit room*/
 int
 checkState(void){
 	if (!strcmp(curr->room->name, exit_room)){
@@ -128,10 +125,9 @@ checkState(void){
 	return 0;
 }
 
-/*Move command takes in movement characters and sets the current room to the valid room*/
 int
 move (char c){
-	if (player_doughnuts <= 0 || player_milkshakes <= 0){ //First checks resource amounts
+	if (player_doughnuts <= 0 || player_milkshakes <= 0){
 		printInfo();
 		printf("lost\n");
 		return 0;
@@ -161,12 +157,13 @@ move (char c){
 			curr->room = curr->room->doors[UP];
 			--player_doughnuts;
 			--player_milkshakes;
-		
 			if (checkState()) { return 0; }
 
 		}
 		else { --player_doughnuts; --player_milkshakes; }
+
 	}
+
 	if (c == 'D'){
 		if (curr->room->doors[DOWN]->name != NULL){
 			curr->room = curr->room->doors[DOWN];
@@ -179,13 +176,12 @@ move (char c){
 	return 1; //True to keep on going
 }
 
-/*Consume method eats doughnuts*/
 int
 consume(void){
-	if (curr->room->num_doughnuts == 0){ //Check room resources first
+	if (curr->room->num_doughnuts == 0){
 		printf("lost\n"); return 0;
 	}
-	if (player_doughnuts > 2){ //Check players capacity
+	if (player_doughnuts > 2){
 		printf("lost\n"); return 0;
 	}
 	curr->room->num_doughnuts = curr->room->num_doughnuts-1;
@@ -193,7 +189,7 @@ consume(void){
 	return 1;
 }
 
-/*Drinks milkshakes*/
+
 int
 drink(void){
 	if (curr->room->num_milkshakes == 0){
@@ -208,7 +204,23 @@ drink(void){
 	return 1;
 }
 
-/*This is the game initalization method and calls the creatRooms and doors functions above*/
+void
+free_list(){
+	struct list *temp;
+	struct list *i = head->next;
+	free(head->room);
+	free(head);
+	//int i = 0;
+	while(i->next!=NULL){
+		temp = i->next;
+		free(i->room);
+		free(i);
+		i = temp;
+	}
+	free(i->room);
+	free(i);
+}
+
 int
 game_setup(void){
 	int number_rooms, number_doors, scan;
@@ -240,124 +252,67 @@ game_setup(void){
 		}
 	}
 	else { printf("error\n"); return(0); }
+
 	return 1; //Return true if all completed 
+
 }
 
-
-/*Game logic is defined within this method, some input checking and calling of specific game commands*/
 void
 run_game(){
+	
 	/*Begin game logic*/
 	int scan; 
 	char command;
 	player_doughnuts = 0;
 	player_milkshakes = 0;
-	scan = scanf("\n%c", &command);
+	scan = scanf("%s", &command);//WHY!
+	//printf("stuff %c\n", command);
+	printInfo();
 	while (scan != EOF){
 		if (scan && (command == 'L' || command == 'U' || command == 'R' || command == 'D' || command == 'G' || command == 'M')){
-			printInfo();//Called at the begining of each loop rather than inbetween
+
 			if (command == 'L' || command == 'U' || command == 'R' || command == 'D'){
 				//printf("%d %d\n", player_doughnuts, player_milkshakes);
 				if (!move(command)) { return; }//If game state is not true return
-				//printInfo();
+				printInfo();
 			}
 			if (command == 'G'){
 				if (!consume()) { return; }
-				//printInfo();
+				printInfo();
 			}
 			if (command == 'M'){
 				if (!drink()) { return; }
-				//printInfo();
+				printInfo();
 			}
-			scan = scanf("\n%c", &command);
+			scan = scanf("%s", &command);
 		}
 		else { printf("error\n"); return; }
 	
 	}
-	printf("lost\n");
-	return;
 }
 
-/*Free list attempts to free all allocated memory. 
- * Unforunately, there is some bizzare behaviour that means
- * not all test cases are working.
- * 
- */
-void
-free_list(){
-	struct list *temp;
-
-//	if(head->next!=NULL){
-//		struct room * lol1 = head->next->room;
-//		if 
-//		struct room * lol2 = head->next->next->room;
-//
-//		printf("not working\n");
-//	}
-//	if(head->next == head->next->next){
-//		printf("an even bigger one\n");
-//	}
-	struct list *i = head->next;
-	//free(head->room);
-	free(head);
-	//int i = 0;
-	while(i->next!=NULL){
-		temp = i->next;
-		free(i->room);
-		free(i);
-		i = temp;
-	}
-	if(i!=head){
-		free(i->room);
-	}
-	free(i);	
-}
-
-
-void
-free_list2() {
-	struct list *item = head;
-	printf("%s", head->next->next->room->name);
-	while (item != NULL) {
-		//delete the room
-		free(item->room);
-		//get the next item before we destroy current item
-		struct list *next_item = item->next;
-		//delete current)
-		free(item);
-		item = next_item;
-	}
-	printf("blah");
-	free(item->room);
-	free(item);
-}
-void
-free_list3(){
-	//struct list *item = true_head;
-	curr = head;
-
-	printf("THIS IS HEAD %s\n", head->room->name);
-	while(curr){
-		//free(item->room);
-		printf("%s\n", curr->room->name);
-		
-		curr = curr->next;
-	}
-}
 
 
 /*Main Method*/
 int
 main(void) {
 
-	if (game_setup()){ //If game setup reuturns, run the game!
+	if (game_setup()){ //If game setup reuturns 
 		run_game();
 	}
+	
+	free_list();
+	/*
+	   curr = head;
+	   while(curr){
+	   printf("Printing name, doughnuts, milkshakes\n");
+	   printf("%s %i %i\n", curr->room->name, curr->room->num_doughnuts, curr->room->num_milkshakes);
+	   curr = curr->next;
+	   }
 
-	free_list();//Free all allocated memory
+*/	
 	//printf("lost\n"); //scan reached EOF and not at winning room
-	if (head != NULL){
-			free(head);
-	}
+
+
 	return 0;
 }
